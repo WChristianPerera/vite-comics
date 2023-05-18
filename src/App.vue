53f7386cdc2fe779801aeefcc0522fb2c935e3e9
WChristianPerera <christianperera13@gmail.com>
2023-05-18 15:11:49 +0200

    Create file

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+
+export default {
+  components: {AppMain, AppFooter, AppHeader},
+};
+
+</script>
+
+<template>
+  <app-header></app-header>
+
+  <main>
+    <app-main></app-main>
+  </main>
+
+  <app-footer></app-footer>
+</template>
+
+<style lang="scss" scoped>
+*{
+  margin: auto;
+  padding: auto;
+  box-sizing: border-box;
+}
+</style>
